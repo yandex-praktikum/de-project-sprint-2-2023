d7e5ff66284f564c1c60d7de38c1b8c8b5ddf796
Dalya <dalya_degtyareva@outlook.com>
2024-02-14 14:14:42 +0300

    Add files via upload

---
+++ step_5.sql
@@ -0,0 +1,22 @@
+CREATE TABLE dwh.customer_report_datamart (
+	id int8 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE) NOT NULL,
+	customer_id int8 NOT NULL,
+	customer_name varchar NOT NULL,
+	customer_address varchar NOT NULL,
+	customer_birthday date NOT NULL,
+	customer_email varchar NOT NULL,
+	customer_money numeric(15, 2) NOT NULL,
+	platform_money int8 NOT NULL,
+	count_order int8 NOT NULL,
+	avg_price_order numeric(10, 2) NOT NULL,
+	median_time_order_completed numeric(10, 1) NULL,
+	top_product_category varchar NOT NULL,
+	top_craftsman_id int8 NOT NULL,
+	count_order_created int8 NOT NULL,
+	count_order_in_progress int8 NOT NULL,
+	count_order_delivery int8 NOT NULL,
+	count_order_done int8 NOT NULL,
+	count_order_not_done int8 NOT NULL,
+	report_period varchar NOT NULL,
+	CONSTRAINT customer_report_datamart_pk PRIMARY KEY (id)
+);
